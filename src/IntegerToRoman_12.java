7414f8e523ed83f1e64803cd2fad568ac654c879
vwslz <vwslzk@gmail.com>
2016-09-25 15:46:28 -0400

    up untili 12

--- src/IntegerToRoman_12.java
+++ src/IntegerToRoman_12.java
@@ -1,4 +1,17 @@
-
+// O(n)
 public class IntegerToRoman_12 {
-
+    public String intToRoman(int num) {
+        int[] ints = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] strs = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String res = "";
+        int i = 0;
+        while (num > 0 && i < ints.length) {
+            while (num >= ints[i]) {
+                res += strs[i];
+                num -= ints[i];
+            }
+            i++;
+        }
+        return res;
+    }
 }
---
+++ src/RomanToInteger_13.java
@@ -0,0 +1,7 @@
+import java.util.Hashtable;
+
+// O()
+public class RomanToInteger_13 {
+    Hashtable<Character, Integer> ht = new Hashtable<Character, Integer>();
+
+}
