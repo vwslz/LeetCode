2f6ad56dadeb7f38f6b5bb17087bd6dfaa07cb90
vwslz <vwslzk@gmail.com>
2016-10-28 20:11:11 -0400

    up untili 901+ 207

--- src/DecodeWays_91.java
+++ src/DecodeWays_91.java
@@ -1,12 +1,28 @@
 // O(n)
 public class DecodeWays_91 {
     public int numDecodings(String s) {
+        if (s == null || s.length() == 0)  return 0;
         char[] c = s.toCharArray();
-        int[] res = new int[c.length];
+        int[] res = new int[c.length + 1];
         res[0] = 1;
-        for (int i = 1; i < c.length; i++) {
-
+        if (s.charAt(0) == '0')
+            return 0;
+        else
+            res[1] = 1;
+        for (int i = 0; i < c.length; i++) {
+            if (s.charAt(i) == '0') {
+                if (i > 0 && (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2'))
+                    res[i + 1] = res[i - 1];
+                else
+                    return 0;
+            }
+            else {
+                if (s.charAt(i - 1) == '1' || (s.charAt(i -1) == '2' &&s.charAt(i) <= '6'))
+                    res[i + 1] = res[i - 1] + res[i];
+                else
+                    res[i + 1] = res[i];
+            }
         }
-        return 0;
+        return res[c.length];
     }
 }
