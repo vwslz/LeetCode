043ac8ad33c7b6a31a05006193ce77a36adc7031
vwslz <vwslzk@gmail.com>
2017-11-29 12:03:54 -0500

    up until 197 (exclude the locked ones and bash)

--- src/NumberOf1Bits_191.java
+++ src/NumberOf1Bits_191.java
@@ -1,5 +1,11 @@
 public class NumberOf1Bits_191 {
     public int hammingWeight(int n) {
-        return 1;
+        int res = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            res += n & 1;
+            n >>= 1;
+        }
+        return res;
     }
 }
---
+++ src/RisingTemperature_197.java
@@ -0,0 +1,8 @@
+public class RisingTemperature_197 {
+    /**
+     * Mysql
+     */
+//    SELECT w1.Id
+//    FROM Weather w1, Weather w2
+//    WHERE TO_DAYS(w1.DATE)=TO_DAYS(w2.DATE)+1 AND w1.Temperature > w2.Temperature
+}
